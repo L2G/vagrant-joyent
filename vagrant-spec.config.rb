28e749c3d4c71c27ebdca85b709e58c0be6a7e97
Larry Gilbert <larry@industrialtoys.com>
2014-12-05 15:19:55 -0800

    Add vagrant-spec config

---
+++ vagrant-spec.config.rb
@@ -0,0 +1,14 @@
+# rubocop:disable Style/FileName
+
+# require_relative 'test/acceptance/base'
+
+def relative_to_me(path)
+  File.expand_path(path, File.dirname(__FILE__))
+end
+
+Vagrant::Spec::Acceptance.configure do |c|
+  c.component_paths << relative_to_me('test/acceptance')
+  c.skeleton_paths << relative_to_me('test/acceptance/skeletons')
+
+  c.provider 'joyent', box: relative_to_me('basebox/joyent-base64.box')
+end
