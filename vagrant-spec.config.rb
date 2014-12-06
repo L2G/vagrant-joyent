dc5606fe11d70aed28fc62e66548d9a7b2af94bb
Larry Gilbert <larry@industrialtoys.com>
2014-12-05 17:17:46 -0800

    Disable test of halt and suspend in vagrant-spec

--- vagrant-spec.config.rb
+++ vagrant-spec.config.rb
@@ -10,5 +10,7 @@ Vagrant::Spec::Acceptance.configure do |c|
   c.component_paths << relative_to_me('test/acceptance')
   c.skeleton_paths << relative_to_me('test/acceptance/skeletons')
 
-  c.provider 'joyent', box: relative_to_me('basebox/joyent-base64.box')
+  c.provider 'joyent',
+             features: ['!halt', '!suspend'],
+             box: relative_to_me('basebox/joyent-base64.box')
 end
