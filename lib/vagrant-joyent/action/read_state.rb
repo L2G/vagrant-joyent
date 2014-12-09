3f90bfbf5c90464ba19082d595d2bda46a77ea53
Larry Gilbert <larry@industrialtoys.com>
2014-12-09 10:41:57 -0800

    Refactor handling of states treated as "not created"

--- lib/vagrant-joyent/action/read_state.rb
+++ lib/vagrant-joyent/action/read_state.rb
@@ -6,6 +6,8 @@ module VagrantPlugins
       # This action reads the state of the machine and puts it in the
       # `:machine_state_id` key in the environment.
       class ReadState
+        NOT_CREATED_STATES = %w(shutting-down terminated)
+
         def initialize(app, env)
           @app    = app
           @logger = Log4r::Logger.new("vagrant_joyent::action::read_state")
@@ -22,7 +24,7 @@ module VagrantPlugins
 
           # Find the machine
           server = joyent.servers.get(machine.id)
-          if server.nil? || [:"shutting-down", :terminated].include?(server.state.to_sym)
+          if server.nil? || NOT_CREATED_STATES.include?(server.state)
             # The machine can't be found
             @logger.info("Machine not found or terminated, assuming it got destroyed.")
             machine.id = nil
